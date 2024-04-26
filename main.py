cadf91679b3dd4d1bf0e43cb6ae55d09950b681a
Elon !Musk <daniel.elon@rub.de>
2024-04-26 14:40:14 +0200

    Update #6

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ print("Josh sagt: Orcas rule!")
 print("The One Piece is Orcas")
 print("just wanna go home now!!!")
 print("Good bye work PC, hello home PC!")
+
+orca_array = ["Free Willy #1", "Free Willy #2", "Free Willy #3"]
+for each in orca_array: pringit commt(f"Jump, {each}!")
