66bca0166fb4087d44685502b21548b8cda4cf0e
Daniel.Elon <daniel.elon@cgm.com>
2024-05-03 12:44:34 +0200

    Database Update

--- database_connection.py
+++ database_connection.py
@@ -1,1 +1,7 @@
 import sqlite3
+
+conn = sqlite3.connect("part_mat_data.db")
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS
+    sensordata(timestamp str, sensorid int, pm25 float, pm10 float, location str)""")
