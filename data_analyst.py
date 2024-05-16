7ae6500e3f89767e70c660edc6f75fabc44ca7c9
Daniel.Elon <daniel.elon@cgm.com>
2024-05-16 16:59:28 +0200

    Data Analyst Class extended #1

--- data_analyst.py
+++ data_analyst.py
@@ -3,5 +3,10 @@ from csv import DictReader
 data = DictReader(open("2021-04-26_sds011_sensor_3659.csv"), delimiter = ";")
 
 print(data.fieldnames)
+sum = 0
+for line in data:
+    print(line)
+    sum += float(line["P1"])
 
-for line in data: print(line)
+avg = sum / (data.line_num - 1)
+print(avg)
