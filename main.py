0ce0c4247defbeb47c94f4aa79f2f1b73aafe7c1
Daniel.Elon <daniel.elon@cgm.com>
2024-05-23 13:41:49 +0200

    Fish Function Update #3

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ print("The One Piece is Orcas")
 print("just wanna go home now!!!")
 print("Good bye work PC, hello home PC!")
 
+fishey = "<oOo><\n"
+
 def whale():
     orca_array = ["Free Willy #1", "Free Willy #2", "Free Willy #3", "Free Willy #n"]
     for each in orca_array: print(f"Jump, {each}!")
-    print("<oOo><\n")
+    print(fishey)
 
 def fish(limit):
     flying_fish = {"Captain": "Oscar", "Admiral": "Rudi", "Rear Admiral": "Carl", "Drunken Sailor": "Alan"}
     for key, value in flying_fish.items(): print(f"Fly, {key} {value}!")
     for i in range(0, limit):
         for each in flying_fish.values(): print(each[0])
-    print("<oOo><\n")
+    print(fishey)
 
 whale()
 fish(1)
