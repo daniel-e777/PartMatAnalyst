f31d403c55e254572a30d5eda779c573e6ac13f3
Daniel.Elon <daniel.elon@cgm.com>
2024-05-23 12:01:52 +0200

    Flying Fish Update in Main

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ print("Good bye work PC, hello home PC!")
 
 orca_array = ["Free Willy #1", "Free Willy #2", "Free Willy #3", "Free Willy #n"]
 for each in orca_array: print(f"Jump, {each}!")
+
+flying_fish = {"Captain": "Oscar", "Admiral": "Rudi", "Rear Admiral": "Carl", "Drunken Sailor": "Alan"}
+for key, value in flying_fish.items(): print(f"Fly, {key} {value}!")
+for each in flying_fish.values(): print(each[0])
