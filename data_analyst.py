ab7fb2cda99890e4ef22af5f7734a7fe1c272250
Daniel.Elon <daniel.elon@cgm.com>
2024-05-28 14:18:15 +0200

    Data Analyst Update

--- data_analyst.py
+++ data_analyst.py
@@ -1,19 +1,24 @@
 from csv import DictReader
 
-data = DictReader(open("2021-04-26_sds011_sensor_3659.csv"), delimiter = ";")
+def evaluation(data):
+    p1_values = []
+    p2_values = []
 
-print(data.line_num)
-print(data.fieldnames)
+    for line in data:
+        print(line)
+        print(data.line_num)
+        p1_values.append(float(line["P1"]))
+        p2_values.append(float(line["P2"]))
 
-p1_sum = 0
-p2_sum = 0
+    p1_avg = sum(p1_values) / (data.line_num - 1)
+    p1_max = max(p1_values)
+    p2_avg = sum(p2_values) / (data.line_num - 1)
+    p2_max = max(p2_values)
+    print(f"Average P1 value: {p1_avg}, max. P1 value: {p1_max}.\nAverage P2 value: {p2_avg}, max. P2 value: {p2_max}")
 
-for line in data:
-    print(line)
-    print(data.line_num)
-    p1_sum += float(line["P1"])
-    p2_sum += float(line["P2"])
+raw_data = DictReader(open("2021-04-26_sds011_sensor_3659.csv"), delimiter = ";")
 
-p1_avg = p1_sum / (data.line_num - 1)
-p2_avg = p2_sum / (data.line_num - 1)
-print(f"Average P1 value: {p1_avg}, average P2 value: {p2_avg}")
+print(raw_data.line_num)
+print(raw_data.fieldnames)
+
+evaluation(raw_data)
